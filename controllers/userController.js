f2e48e17209106d87b8377ba91b03bb49fc3dc0a
Jimmy404 <bruno_pier@hotmail.com>
2020-11-03 20:09:29 -0300

    Password protection in DB - HASH

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,22 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.newUser = async (req, res) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
+
     let user = await User.findOne({ email });
+
     if(user){
         return res.status(400).json({msg: 'User already exist'})
     }
-    user = await new User(req.body);
-    user.save();
-    res.json({msg: 'user created successfully'});
+
+    user = new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    try {
+        await user.save();
+        res.json({msg: 'user created successfully'});
+    } catch (error) {
+        console.log(error);
+    }
 }
