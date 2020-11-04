54dc9bb490635f2ee8a6516dbb15841e2d068a36
Jimmy404 <bruno_pier@hotmail.com>
2020-11-04 18:38:47 -0300

    Creating the schema for the links

---
+++ models/Link.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const linksSchema = new Schema({
+    url: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    original_name: {
+        type: String,
+        required: true
+    },
+    downloads: {
+        type: Number
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        default: null
+    },
+    password: {
+        type: String
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Links', linksSchema);
