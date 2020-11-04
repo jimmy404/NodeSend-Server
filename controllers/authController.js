f1a2d6faa241b5de8238e4e0448a41d88dec3d44
Jimmy404 <bruno_pier@hotmail.com>
2020-11-04 00:16:22 -0300

    Check if a user exists

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,17 @@
+const User = require('../models/User');
 
 exports.authenticateUser = async (req, res, next) => {
-
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    console.log(user);
+    
+    if (!user) {
+        res.status(401).json({msg: 'Username does not exist'})
+        return next();
+    }
+    console.log('Username exist')
 }
 
 exports.authenticatedUser = (req, res) => {
-    
+
 }
