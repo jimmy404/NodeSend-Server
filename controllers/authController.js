84c09f6ece72799ea7322263fb52537fafb88b36
Jimmy404 <bruno_pier@hotmail.com>
2020-11-04 00:54:47 -0300

    Creates a JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,32 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' })
 
 exports.authenticateUser = async (req, res, next) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
-    console.log(user);
-    
+
     if (!user) {
         res.status(401).json({msg: 'Username does not exist'})
         return next();
     }
-    console.log('Username exist')
+
+
+    if (bcrypt.compareSync(password, user.password)) {
+        console.log('Password Correct!')
+        const token = jwt.sign({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        }, process.env.SECRET, {
+            expiresIn: '8h'
+        });
+        res.json({token});
+    } else {
+        res.status(401).json({msg: "Incorrect Password"})
+    }
+
 }
 
 exports.authenticatedUser = (req, res) => {
