4d16e42f029d13dd84530efc2685c0f5e036a909
Jimmy404 <bruno_pier@hotmail.com>
2020-11-05 13:31:22 -0300

    Adds and configure Multer - Folder - Routes - Controllers

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,7 +1,7 @@
 
 
 exports.uploadFile = async (req, res) => {
-
+    console.log(req.file);
 }
 
 exports.deleteFile = async (req, res) => {
--- routes/files.js
+++ routes/files.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const filesController = require('../controllers/filesController');
 const auth = require('../middleware/auth');
 
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
+
 router.post('/',
+    upload.single('file'),
     filesController.uploadFile
 );
 
