304e7dba5262214c905d3aa12130e7add8254eb8
Jimmy404 <bruno_pier@hotmail.com>
2020-11-11 12:34:13 -0300

    Adds name as link response

--- controllers/linksController.js
+++ controllers/linksController.js
@@ -10,11 +10,11 @@ exports.newLink = async(req, res, next) => {
         return res.status(400).json({ errors: errors.array()});
     }
 
-    const { original_name } = req.body;
+    const { original_name, name } = req.body;
 
     const link = new Links();
     link.url = shortid.generate();
-    link.name = shortid.generate();
+    link.name = name;
     link.original_name = original_name;
 
     if(req.user){
